1a160bae3391202a3419342caf853a422ab462d2
Malik Bougacha <bougacha@google.com>
2020-09-16 16:29:48 +0200

    add more request on the geoserver for updating layer
    The request goes as follow:
    1) create the import task
    2) using the import task add the file
    3) start the import job

--- importer/main.py
+++ importer/main.py
@@ -6,31 +6,39 @@ import argparse
 import os
 
 
-def post(user, password):
-    url = "http://localhost:8000/geoserver/rest/imports?async=true"
+def post(user, password, file_path):
+    url = "http://localhost:8000/geoserver/rest/imports"
     payload = {
        "import": {
           "targetWorkspace": {
              "workspace": {
-            "name": "topp"
+            "name": "enermaps"
              }
           },
-          "data": {
-            "type": "remote",
-            "location": "ftp://myserver/data/bc_shapefiles",
-            "username": "dan",
-            "password": "secret"
-          }
        }
     }
-    requests.get(url, auth=(user, password), json=payload)
+    session = requests.Session()
+    session.auth = (user, password)
+    resp = session.post(url, json=payload)
+    print(resp, resp.text)
+    resp_payload = resp.json()
+    import_id = resp_payload["import"]["id"]
+
+    filename = os.path.basename(file_path)
+    url = "http://localhost:8000/geoserver/rest/imports/{!s}/tasks".format(import_id)
+    with open(file_path) as shapefile:
+        resp = session.get(url, data={"name": filename, "filedata": shapefile.read()})
+        print(resp, resp.text)
+    resp = session.post("http://localhost:8000/geoserver/rest/imports/{!s}".format(import_id))
+    print(resp, resp.text)
+
 
 
 def get_parser():
     """return the cli command parser
     """
     parser = argparse.ArgumentParser(__name__, description="import a raster into a geoserver")
-    parser.add_argument("-u", "--user", default="geoserver")
+    parser.add_argument("-u", "--user", default="admin")
     parser.add_argument("shapefile")
     return parser
 
@@ -40,7 +48,7 @@ def main():
 
     parser = get_parser()
     args = parser.parse_args()
-    post(user=args.user, password=os.environ['GEOSERVER_PASS'])
+    post(user=args.user, password=os.environ['GEOSERVER_PASS'], file_path=args.shapefile)
 
 if __name__ == "__main__":
     main()
