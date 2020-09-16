ef136ef2dfbe2bf447645d618657968fcc66b075
Malik Bougacha <bougacha@google.com>
2020-09-16 16:47:35 +0200

    reformat

--- importer/main.py
+++ importer/main.py
@@ -8,15 +8,7 @@ import os
 
 def post(user, password, file_path):
     url = "http://localhost:8000/geoserver/rest/imports"
-    payload = {
-       "import": {
-          "targetWorkspace": {
-             "workspace": {
-            "name": "enermaps"
-             }
-          },
-       }
-    }
+    payload = {"import": {"targetWorkspace": {"workspace": {"name": "enermaps"}}}}
     session = requests.Session()
     session.auth = (user, password)
     resp = session.post(url, json=payload)
@@ -29,15 +21,18 @@ def post(user, password, file_path):
     with open(file_path) as shapefile:
         resp = session.post(url, files={"name": filename, "filedata": shapefile})
         print(resp, resp.text)
-    resp = session.post("http://localhost:8000/geoserver/rest/imports/{!s}".format(import_id))
+    resp = session.post(
+        "http://localhost:8000/geoserver/rest/imports/{!s}".format(import_id)
+    )
     print(resp, resp.text)
-
 
 
 def get_parser():
     """return the cli command parser
     """
-    parser = argparse.ArgumentParser(__name__, description="import a raster into a geoserver")
+    parser = argparse.ArgumentParser(
+        __name__, description="import a raster into a geoserver"
+    )
     parser.add_argument("-u", "--user", default="admin")
     parser.add_argument("shapefile")
     return parser
@@ -48,7 +43,10 @@ def main():
 
     parser = get_parser()
     args = parser.parse_args()
-    post(user=args.user, password=os.environ['GEOSERVER_PASS'], file_path=args.shapefile)
+    post(
+        user=args.user, password=os.environ["GEOSERVER_PASS"], file_path=args.shapefile
+    )
+
 
 if __name__ == "__main__":
     main()
