10c30a1fac19ae5d0dfc08686715c4f7ba808677
Malik Bougacha <bougacha@google.com>
2020-09-22 15:40:27 +0200

    only wait the minimum amount of time for the geoserver to come up

--- importer/test_import.py
+++ importer/test_import.py
@@ -1,17 +1,39 @@
 """Integration test for the importer
 """
+import logging
 import os
 import subprocess
 import time
 import unittest
 
+import requests
+from dotenv import load_dotenv
+
 import geoserver_init
 import main
-from dotenv import load_dotenv
 
 DOCKER_COMPOSE_PROJECT = "import_integration_test"
 DOCKER_COMPOSE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 GEOSERVER_BASE_URL = "http://127.0.0.1:8000/geoserver"
+GEOSERVER_MAX_RETRY = 10
+GEOSERVER_WAIT_SEC = 4
+
+
+def wait_for_geoserver():
+    print("Waiting for geoserver")
+    for _ in range(GEOSERVER_MAX_RETRY):
+        try:
+            requests.get(GEOSERVER_BASE_URL)
+        except requests.exceptions.ConnectionError:
+            print(".", end="", flush=True)
+            time.sleep(GEOSERVER_WAIT_SEC)
+        else:
+            return
+    raise Exception(
+        "Couldn't reach the geoserver in {!s} seconds".format(
+            GEOSERVER_MAX_RETRY * GEOSERVER_WAIT_SEC
+        )
+    )
 
 
 def docker_compose(*args):
@@ -21,7 +43,7 @@ def docker_compose(*args):
     command += ["--project-name", DOCKER_COMPOSE_PROJECT]
     command += ["--no-ansi"]
     command += args
-    print("running", command)
+    logging.info("running", command)
     subprocess.check_output(command)
 
 
@@ -59,7 +81,7 @@ def setUpModule():
     docker_compose("down")
     docker_compose("rm")
     docker_compose("up", "--build", "-d", "geoserver", "db")
-    time.sleep(60)
+    wait_for_geoserver()
     # init the geoserver
     geoserver_init.init()
 
