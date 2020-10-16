e101a1da752d61c035e4d43499eef43a495adab0
Malik Bougacha <bougacha@google.com>
2020-10-16 10:47:54 +0200

    add more argument in the import functino

--- importer/main.py
+++ importer/main.py
@@ -4,7 +4,6 @@
 import argparse
 import logging
 import os
-import posixpath
 from collections import defaultdict
 
 import requests
@@ -17,53 +16,53 @@ class GeoserverImportError(Exception):
     pass
 
 
-DATASTORE = {"dataStore": {"name": "geoserver_db"}}
+class GeoserverImporter:
+    DATASTORE = {"dataStore": {"name": "geoserver_db"}}
 
+    def __init__(self, base_url, user, password):
+        self.session = requests.Session()
+        self.session.auth = (user, password)
+        self.base_url = base_url.rstrip("/")
 
-def get_import_creation_payload(workspace_name):
-    import_creation_payload = NestedDict
-    import_creation_payload["import"]["targetWorkspace"]["workspace"][
-        "name"
-    ] = workspace_name
-    import_creation_payload["targetStore"] = DATASTORE
-    return import_creation_payload
+    def getImportCreationPayload(self, workspace_name):
+        import_creation_payload = NestedDict
+        import_creation_payload["import"]["targetWorkspace"]["workspace"][
+            "name"
+        ] = workspace_name
+        import_creation_payload["targetStore"] = self.DATASTORE
+        return import_creation_payload
 
-
-def import_file(base_url, user, password, file_path, workspace_name, is_raster=False):
-    session = requests.Session()
-    session.auth = (user, password)
-    import_creation_payload = get_import_creation_payload(workspace_name)
-    url = posixpath.join(base_url, "rest/imports")
-    resp = session.post(url, json=import_creation_payload)
-    if not resp.ok:
-        raise GeoserverImportError(resp.text)
-    resp_payload = resp.json()
-    import_id = resp_payload["import"]["id"]
-
-    filename = os.path.basename(file_path)
-    url = posixpath.join(base_url, "rest/imports/{!s}/tasks".format(import_id))
-    with open(file_path, "br") as geofile:
-        resp = session.post(url, files={"name": filename, "filedata": geofile})
-        logging.info(resp, resp.text)
+    def import_file(self, file_path, workspace_name, is_raster=False, projection=None):
+        import_creation_payload = self.getImportCreationPayload(workspace_name)
+        url = self.base_url + "/rest/imports"
+        resp = self.session.post(url, json=import_creation_payload)
         if not resp.ok:
             raise GeoserverImportError(resp.text)
+        resp_payload = resp.json()
+        import_id = resp_payload["import"]["id"]
 
-    if not is_raster:
-        # the file doesn't have a store set, so it will default to the default store
-        # currently we don't support importing raster file to postgis directly
-        url = posixpath.join(
-            base_url, "rest/imports/{!s}/tasks/0/target".format(import_id)
-        )
-        resp = session.put(url, json=DATASTORE)
+        filename = os.path.basename(file_path)
+        url = self.base_url + "/rest/imports/{!s}/tasks".format(import_id)
+        with open(file_path, "br") as geofile:
+            resp = self.session.post(url, files={"name": filename, "filedata": geofile})
+            logging.info(resp, resp.text)
+            if not resp.ok:
+                raise GeoserverImportError(resp.text)
+
+        if not is_raster:
+            # the file doesn't have a store set, so it will default to the default store
+            # currently we don't support importing raster file to postgis directly
+            url = self.base_url + "/rest/imports/{!s}/tasks/0/target".format(import_id)
+            resp = self.session.put(url, json=self.DATASTORE)
+            if not resp.ok:
+                raise GeoserverImportError(resp.text)
+            logging.info(resp, resp.text)
+
+        url = self.base_url + "/rest/imports/{!s}".format(import_id)
+        resp = self.session.post(url)
         if not resp.ok:
             raise GeoserverImportError(resp.text)
         logging.info(resp, resp.text)
-
-    url = posixpath.join(base_url, "rest/imports/{!s}".format(import_id))
-    resp = session.post(url)
-    if not resp.ok:
-        raise GeoserverImportError(resp.text)
-    logging.info(resp, resp.text)
 
 
 def get_parser():
@@ -71,7 +70,6 @@ def get_parser():
     parser = argparse.ArgumentParser(
         __name__, description="import a raster into a geoserver"
     )
-    parser.add_argument("-u", "--user", default="admin")
     parser.add_argument("-n", "--workspace_name", default="enermaps")
     parser.add_argument("-r", "--is_raster", default="False")
     parser.add_argument(
@@ -93,10 +91,12 @@ def main():
     args = parser.parse_args()
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
-    import_file(
-        base_url=args.base_url,
-        user=args.user,
+    geoserver_importer = GeoserverImporter(
+        user=os.environ["GEOSERVER_USER"],
         password=os.environ["GEOSERVER_PASS"],
+        base_url=args.base_url,
+    )
+    geoserver_importer.import_file(
         file_path=args.geofile,
         workspace_name=args.workspace_name,
         is_raster=args.is_raster,
